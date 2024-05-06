ba9b9e72201a7683c3ab95ccbb9dfdb4732fdc00
MintOwlTech <mintowltech@gmail.com>
2024-05-06 22:28:42 +0200

    Pull the Formik Context to a higher component to make ModuleStack Sync Work

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,136 @@ import { useState } from "react";
 import "./App.css";
 import { Box, Grid, Typography } from "@mui/material";
 import Header from "./UI/Header";
+import { Formik, Form, Field } from "formik";
+import NewFlexibleFormField from "./UI/FormComponents/NewFlexibleFormField";
 
 import ModuleStack from "./UI/ModuleStack";
 import NewDynamicFormBuilder from "./UI/NewDynamicFormBuilder";
+import newQuestions from "./choicesJSON/newQuestionsFormat.json";
+import { capitalizeFirstLetter } from "./helperFunctions/OtherHelper";
+import { handleFileChange, handleSubmit } from "./helperFunctions/FormHelper";
+
 
 function App() {
-  const [count, setCount] = useState(0);
+ 
+  // Generate initial values from questions data
+  const initialValues = Object.values(newQuestions).reduce(
+    (values, question) => {
+      values[question.id] = ""; // Set initial value as empty
+      return values;
+    },
+    {}
+  );
+
+  
 
   return (
     <>
       <Header />
-
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {(FormikProps ) => (
       <Grid container direction={"row"}>
         <Grid item xs={4} className="stackContainer">
           <Grid item className="stackFrame">
-            <ModuleStack />
+         
+            <ModuleStack FormikProps={FormikProps}/>
           </Grid>
         </Grid>
         <Grid item xs={8} className="questionairContainer">
           <Grid item className="questionairFrame">
-            <NewDynamicFormBuilder />
+          <div>
+      <h1>Experiment Builder</h1>
+     
+          <Form>
+            {/* Optional JSON File upload */}
+            <Typography>Optionally upload existing config File</Typography>
+            <input
+              type="file"
+              id="uploadedJson"
+              accept=".json"
+              onChange={handleFileChange}
+            />
+            {/* For each Folder */}
+            {Object.entries(newQuestions).map(([folder, folderContent]) => (
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <h4> {capitalizeFirstLetter(folder)}</h4>
+
+                {Object.entries(folderContent).map(([file, fileContent]) => (
+                  <>
+                    {" "}
+                    {Object.entries(fileContent).map(
+                      ([className, fileContent]) => (
+                        <label>
+                          <Field
+                            type="checkbox"
+                            name={folder}
+                            value={className}
+                          ></Field>
+                          {className}
+                        </label>
+                      )
+                    )}
+                  </>
+                ))}
+
+                {/* For each Filename */}
+                {Object.entries(folderContent).map(([file, fileContent]) => (
+                  <>
+                    {/* For each Class in the File */}
+                    {Object.entries(fileContent).map(
+                      ([className, classContent]) => (
+                        <div style={{ paddingLeft: "16px" }}>
+                          {FormikProps.values[folder] &&
+                            FormikProps.values[folder].includes(className) && (
+                              <div
+                                style={{
+                                  display: "flex",
+                                  flexDirection: "column",
+                                  border: "2px solid #000000",
+                                  borderRadius: "10px",
+                                  padding: "8px",
+                                  margin: "8px",
+                                }}
+                              >
+                                <p>{className} Params</p>
+
+                                {/* Conditionally renders Param Questions if the class is selected */}
+
+                                {Object.entries(classContent).map(
+                                  ([Param, value]) => (
+                                    <>
+                                      <label htmlFor={`${className}:${Param}`}>
+                                        {Param}:
+                                      </label>
+                                      <NewFlexibleFormField
+                                        id={`${className}:${Param}`}
+                                        object={value}
+                                        type={value.type}
+                                        name={`${className}:${Param}`}
+                                        label={Param}
+                                      />
+                                    </>
+                                  )
+                                )}
+                              </div>
+                            )}
+                        </div>
+                      )
+                    )}
+                  </>
+                ))}
+              </div>
+            ))}
+
+            <button type="submit">Submit</button>
+          </Form>
+        
+    </div>
           </Grid>
         </Grid>
       </Grid>
+      )}
+      </Formik>
     </>
   );
 }
--- src/UI/ModuleStack.jsx
+++ src/UI/ModuleStack.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import { Stack, Paper, List, ListItem, ListItemText, Typography, Container } from '@mui/material';
-import questions from '../choicesJSON/newQuestionsFormat.json';
-import FlexibleBlock from './ModuleStackComponents/FlexibleBlock';
+import React from "react";
+import {
+  Stack,
+  Paper,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Container,
+} from "@mui/material";
+import questions from "../choicesJSON/newQuestionsFormat.json";
+import FlexibleBlock from "./ModuleStackComponents/FlexibleBlock";
 
-function ModuleStack() {
+function ModuleStack({ FormikProps }) {
   return (
     <Container maxWidth="md">
       <Typography variant="h4" align="center" gutterBottom>
         Your ReproModel Structure
       </Typography>
-      <Stack spacing={2} >
-      {Object.entries(questions).map(
-                      ([folder, folderContent]) => (
-                        <FlexibleBlock key={folder} status={"active"} name={folder}/>
-                      )
-                    )}
-        
+      <Stack spacing={2}>
+        {Object.entries(questions).map(([folder, folderContent]) => (
+          <>
+            {
+              FormikProps.touched && FormikProps.touched[folder] ? (
+                <FlexibleBlock key={folder} status={"active"} name={folder} />
+              ) : (
+                <FlexibleBlock key={folder} status={"passive"} name={folder} />
+              ) // This is the else case
+            }
+          </>
+        ))}
       </Stack>
     </Container>
   );
