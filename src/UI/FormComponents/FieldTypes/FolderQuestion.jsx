2268123bd3294732777ca3c4f077a430a86488d9
MintOwlTech <mintowltech@gmail.com>
2024-05-17 08:59:54 +0200

    Implement Dropdown with Typeahead

--- src/UI/FormComponents/FieldTypes/FolderQuestion.jsx
+++ src/UI/FormComponents/FieldTypes/FolderQuestion.jsx
@@ -1,47 +1,43 @@
 import React from "react";
 import { Field } from "formik";
+import CustomSelect from "./CustomSelectComponent";
+
+const extractSecondLevelKeys = (obj) => {
+  const options = [];
+
+  const traverse = (obj, level = 0) => {
+    if (level === 1) {
+      Object.keys(obj).forEach((key) => {
+        options.push({ value: key, label: key });
+      });
+    } else {
+      Object.values(obj).forEach((value) => {
+        if (typeof value === "object") {
+          traverse(value, level + 1);
+        }
+      });
+    }
+  };
+
+  traverse(obj);
+  return options;
+};
 
 export function SmartFolderField({ folder, folderContent }) {
-    // Define the array of specific folder names
-    const multipleSelectFolders = ["models"];
+  // Define the array of specific folder names
+  const multipleSelectFolders = ["models", "metrics"];
 
-    // Check if the folder name is within the specific folders array
-    const isMultipleFolder = multipleSelectFolders.includes(folder);
-    return (
-     <>
-     
-     {isMultipleFolder ? (
-                // If folder is one of the specific ones, render this block
-                Object.entries(folderContent).map(([file, fileContent]) => (
-                    <div style={{ display: "flex", flexDirection: "column" }} key={file}> {/* Always use unique keys for list items in React */}
-                        {Object.entries(fileContent).map(([className, fileDetails]) => (
-                            <label key={className}> {/* Unique key for child list */}
-                                <Field
-                                    type="checkbox"
-                                    name={folder}
-                                    value={className}
-                                />
-                                {className}
-                            </label>
-                        ))}
-                    </div>
-                ))
-            ) : (
-                // If folder is not one of the specific ones, render an alternative component
-                <Field key={folder} as="select" id={folder} name={folder}>
-                  <option value="">Select an option</option>
-                  {Object.entries(folderContent).map(([file, fileContent]) => (
-                    <>
-                      {" "}
-                      {Object.entries(fileContent).map(
-                        ([className, fileContent]) => (
-                          <option value={className}>{className}</option>
-                        )
-                      )}
-                    </>
-                  ))}
-                </Field>
-            )}
-     </>
-    );
-  }
+  // Convert folderContent into Array
+  const newoptions = extractSecondLevelKeys(folderContent);
+
+  // Check if the folder name is within the specific folders array
+  const isMultipleFolder = multipleSelectFolders.includes(folder);
+  return (
+    <CustomSelect
+      name={folder}
+      options={newoptions}
+      placeholder="Select or start typing"
+      isMulti={isMultipleFolder}
+    />
+  );
+}
--- src/UI/NewDynamicFormBuilder.jsx
+++ src/UI/NewDynamicFormBuilder.jsx
@@ -72,15 +72,15 @@ const DynamicFormBuilder = ({
                                 ([Param, value]) => (
                                   <>
                                     <label
-                                      htmlFor={`${folder}_params:${className}:${Param}`}
+                                      htmlFor={`${folder}_params:${file}:${className}:${Param}`}
                                     >
                                       {Param}:
                                     </label>
                                     <NewFlexibleFormField
-                                      id={`${folder}_params:${className}:${Param}`}
+                                      id={`${folder}_params:${file}:${className}:${Param}`}
                                       object={value}
                                       type={value.type}
-                                      name={`${folder}_params:${className}:${Param}`}
+                                      name={`${folder}_params:${file}:${className}:${Param}`}
                                       label={Param}
                                     />
                                   </>
@@ -109,104 +109,6 @@ const DynamicFormBuilder = ({
                 </>
               ))}
             </div>
-          ) : folder === "metrics" ? (
-
-            <>
-
-            {/** Currently doesn't do anything since the metric is catched above */}
-              {Object.entries(folderContent).map(([file, fileContent]) => (
-                <div
-                  style={{ display: "flex", flexDirection: "column" }}
-                  key={file}
-                >
-                  {file !== "torchmetricswrewe" ? (
-                    <>
-                      {Object.entries(fileContent).map(
-                        ([className, fileDetails]) => (
-                          <label key={className}>
-                            {" "}
-                            {/* Unique key for child list */}
-                            <Field
-                              type="checkbox"
-                              name={folder}
-                              value={className}
-                            />
-                            {className}
-                          </label>
-                        )
-                      )}
-                    </>
-                  ) : (
-                    <>
-                      <label key="torchmetrics">
-                        <Field
-                          type="checkbox"
-                          name={folder}
-                          value="TorchMetrics"
-                        />
-                        TorchMetrics
-                      </label>
-                    </>
-                  )}
-                </div>
-              ))}
-
-              {/** Handle the normal options beside Torchmetrics */}
-              {/* For each Filename */}
-              {Object.entries(folderContent).map(([file, fileContent]) => (
-                <>
-                  {/* For each Class in the File */}
-                  {Object.entries(fileContent).map(
-                    ([className, classContent]) => (
-                      <div style={{ paddingLeft: "16px" }}>
-                        {FormikProps.values[folder] &&
-                          FormikProps.values[folder].includes(className) && (
-                            <div className="paramBox">
-                              <p>{className} Params</p>
-
-                              {/* Conditionally renders Param Questions if the class is selected or torchmetric Options */}
-
-                              <>
-                                {Object.entries(classContent).map(
-                                  ([Param, value]) => (
-                                    <>
-                                      <label
-                                        htmlFor={`${folder}_params:${className}:${Param}`}
-                                      >
-                                        {Param}:
-                                      </label>
-                                      <NewFlexibleFormField
-                                        id={`${folder}_params:${className}:${Param}`}
-                                        object={value}
-                                        type={value.type}
-                                        name={`${folder}_params:${className}:${Param}`}
-                                        label={Param}
-                                      />
-                                    </>
-                                  )
-                                )}{" "}
-                              </>
-                            </div>
-                          )}
-                      </div>
-                    )
-                  )}
-                </>
-              ))}
-
-              {/** Handle the TorchMetric Options */}
-              {FormikProps.values[folder] &&
-                FormikProps.values[folder].includes("TorchMetrics") && (
-                  <div style={{ paddingLeft: "16px" }}>
-                    <CustomSelect
-                      name="flavor"
-                      options={options}
-                      placeholder="Select a flavor"
-                      isMulti={false}
-                    />
-                  </div>
-                )}
-            </>
           ) : (
             <>
               <label htmlFor={`${folder}`}>{folder}:</label>
