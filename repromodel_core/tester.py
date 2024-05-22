b9f07390460f80efa3045094db48f4880e3d441f
dsitnik <dario.sitnik@gmail.com>
2024-05-22 11:54:25 +0200

    RM-011 tester w/ ckpt inference from train_config

--- repromodel_core/tester.py
+++ repromodel_core/tester.py
@@ -7,12 +7,15 @@ from tqdm import tqdm
 from easydict import EasyDict as edict
 import argparse
 from src.getters import configure_component
-from src.utils import print_to_file, load_state
+from src.utils import print_to_file, load_state, get_all_ckpts, delete_command_outputs, TqdmFile
 
 SRC_DIR = "src."
 
 # Main testing function
 def test(input_data):
+    # Reset the console output file
+    delete_command_outputs()
+    
     # Load config
     if os.path.isfile(input_data):
         with open(input_data, 'r') as file:
@@ -33,52 +36,59 @@ def test(input_data):
     # TensorBoard writer
     writer = SummaryWriter(log_dir=cfg.tensorboard_log_path)
 
-    for model_name in cfg.models:
-        print_to_file(f"Testing model: {model_name}")
+
+    # get all saved checkpoints
+    checkpoints = get_all_ckpts(cfg.model_save_path, cfg.models, cfg.data_splits.k)
+    for m, model_name in enumerate(cfg.models):
+        # Custom file object for TQDM
+        tqdm_file = TqdmFile(config=cfg, model_num = m)
+
         model_path = SRC_DIR + "models." + model_name[0].lower() + model_name[1:]
         model_params = cfg.models_params[model_name]
-        checkpoint_path = cfg.checkpoints[model_name]
+        checkpoint_path = checkpoints[model_name]
         # Load model
         model = configure_component(model_path, model_name, model_params).to(cfg.device)
 
-        # Load model from checkpoint
-        checkpoint = torch.load(checkpoint_path, map_location=cfg.device)
-        model = load_state(model, checkpoint)
+        #add iteration over all folds
+        for k in range(cfg.data_splits.k):
+            print_to_file(f"Testing model {model_name} on fold {k}")
+            checkpoint = torch.load(checkpoint_path[k], map_location=cfg.device)
+            model = load_state(model, checkpoint)
 
-        # Configure metrics
-        metrics = {}
-        for metric_name in cfg.metrics:
-            params = cfg.metrics_params[metric_name]
-            metric_path = SRC_DIR + "metrics." + metric_name[0].lower() + metric_name[1:]
-            metrics[metric_name] = configure_component(metric_path, metric_name, params)
+            # Configure metrics
+            metrics = {}
+            for metric_name in cfg.metrics:
+                params = cfg.metrics_params[metric_name]
+                metric_path = SRC_DIR + "metrics." + metric_name[0].lower() + metric_name[1:]
+                metrics[metric_name] = configure_component(metric_path, metric_name, params)
 
-        # Testing loop
-        model.eval()
-        total_metrics = {metric_name: 0.0 for metric_name in cfg.metrics}
-        num_samples = 0
+            # Testing loop
+            model.eval()
+            total_metrics = {metric_name: 0.0 for metric_name in cfg.metrics}
+            num_samples = 0
 
-        with torch.no_grad():
-            for batch_idx, batch in enumerate(tqdm(test_loader)):
-                inputs, targets = batch
-                inputs, targets = inputs.to(cfg.device), targets.to(cfg.device)
-                outputs = model(inputs)
+            with torch.no_grad():
+                progress_bar = tqdm(enumerate(test_loader), total=len(test_loader), file=tqdm_file)
+                for batch_idx, (inputs, targets) in progress_bar:
+                    inputs, targets = inputs.to(cfg.device), targets.to(cfg.device)
+                    outputs = model(inputs)
 
-                batch_size = inputs.size(0)
-                num_samples += batch_size
+                    batch_size = inputs.size(0)
+                    num_samples += batch_size
 
-                for metric_name, metric in metrics.items():
-                    batch_metric_value = metric(outputs, targets)
-                    total_metrics[metric_name] += batch_metric_value * batch_size
+                    for metric_name, metric in metrics.items():
+                        batch_metric_value = metric(outputs, targets)
+                        total_metrics[metric_name] += batch_metric_value * batch_size
 
-        # Compute average metrics
-        avg_metrics = {metric_name: value / num_samples for metric_name, value in total_metrics.items()}
+            # Compute average metrics
+            avg_metrics = {metric_name: value / num_samples for metric_name, value in total_metrics.items()}
 
-        # Log results to TensorBoard
-        for metric_name, value in avg_metrics.items():
-            writer.add_scalar(f'Test/{model_name}/{metric_name}', value)
+            # Log results to TensorBoard
+            for metric_name, value in avg_metrics.items():
+                writer.add_scalar(f'Test/Fold_{k}/{model_name}/{metric_name}', value)
         
     writer.close()
-    print_to_file("Testing completed and results logged to TensorBoard successfully.")
+    print_to_file("Testing is completed and results are logged to TensorBoard successfully.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Test multiple models")
