e76f5000b116b52fb7757135001d778bb8a11056
tomonarifeehan <feehan.tomonari@gmail.com>
2024-05-26 12:40:12 -0400

    cleaned up FlexibleBlock component

--- src/UI/ModuleStackComponents/FlexibleBlock.jsx
+++ src/UI/ModuleStackComponents/FlexibleBlock.jsx
@@ -1,68 +1,67 @@
-import React from "react";
-import { Paper, Typography } from "@mui/material";
-import { capitalizeFirstLetter } from "../../helperFunctions/OtherHelper";
+import React from "react"
 
+import { capitalizeFirstLetter } from "../../helperFunctions/OtherHelper"
+import { Paper, Typography } from "@mui/material"
 
 function ActiveBlock({ id, name, status }) {
   return (
     <Paper
-      key={id}
-      variant="outlined"
-      sx={{
-        backgroundColor: "#95e9fb",
-        p: 0.5,
-        boxShadow: "0 0 10px rgba(0, 255, 0, 0.5)",
-      }}
+      key = { id }
+      variant = "outlined"
+      sx = { { backgroundColor: "#95e9fb", p: 0.5, boxShadow: "0 0 10px rgba(0, 255, 0, 0.5)" } }
     >
-      <Typography align="center" variant="h6" gutterBottom>
-      {capitalizeFirstLetter(name)}
-      </Typography>
+      <Typography align = "center" variant = "h6" gutterBottom>{ capitalizeFirstLetter(name) }</Typography>
     </Paper>
-  );
+  )
 }
 
 function CompletedBlock({ id, name, status }) {
   return (
     <Paper
-      key={id}
-      variant="outlined"
-      sx={{ backgroundColor: "#dff9fe", p: 0.5 }}
+      key = { id }
+      variant = "outlined"
+      sx = { { backgroundColor: "#dff9fe", p: 0.5 } }
     >
-      <Typography align="center" variant="h6" gutterBottom>
-        {capitalizeFirstLetter(name)}
-      </Typography>
+      <Typography align = "center" variant = "h6" gutterBottom>{ capitalizeFirstLetter(name) }</Typography>
     </Paper>
-  );
+  )
 }
 
-function PassivBlock({ id, name, status }) {
+function PassiveBlock({ id, name, status }) {
   return (
     <Paper
-      key={id}
-      variant="outlined"
-      sx={{ backgroundColor: "#e6f0f1", p: 0.5 }}
+      key = { id }
+      variant = "outlined"
+      sx = { { backgroundColor: "#e6f0f1", p: 0.5 } }
     >
-      <Typography align="center" variant="h6" gutterBottom>
-      {capitalizeFirstLetter(name)}
-      </Typography>
+      <Typography align = "center" variant = "h6" gutterBottom>{ capitalizeFirstLetter(name) }</Typography>
     </Paper>
-  );
+  )
 }
 
 function FlexibleBlock({ status, name }) {
+  
   const renderSwitch = () => {
+    
     switch (status) {
+      
       case "active":
-        return <ActiveBlock id={name} name={name} status={status} />;
+        return <ActiveBlock id = { name } name = { name } status = { status } />
+      
       case "completed":
-        return <CompletedBlock id={name} name={name} status={status} />;
+        return <CompletedBlock id = { name } name = { name } status = { status } />
+      
       case "passive":
-        return <PassivBlock id={name} name={name} status={status} />;
+        return <PassiveBlock id = { name } name = { name } status = { status } />
+      
       default:
-        return <div>Error occured, unsupported type</div>;
+        return <div>Error occured, unsupported type</div>
+
     }
-  };
-  return <>{renderSwitch()}</>;
+
+  }
+
+  return <>{ renderSwitch() }</>
 }
 
-export default FlexibleBlock;
+export default FlexibleBlock
