44ca5f9320cc1f5c4bb1e85ec92741727d163548
tomonarifeehan <feehan.tomonari@gmail.com>
2024-05-26 15:01:31 -0400

    refactored Progress Viewer files

--- src/UI/TrainingViewer.jsx
+++ src/UI/TrainingViewer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import FileDropdown from "./ViewerComponents/FileDropdown";
-import ProgressFileReader from "./ViewerComponents/ProgressFileReader";
+import FileDropdown from "../components/progress-viewer/file-dropdown";
+import ProgressFileReader from "../components/progress-viewer/progress-file-reader";
 
 
 const TrainingViewer = ({}) => {
