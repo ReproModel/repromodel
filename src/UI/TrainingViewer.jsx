66ae12ab9ccd4655bf41d0f8e6ce87714ef95f9a
tomonarifeehan <feehan.tomonari@gmail.com>
2024-05-26 12:07:31 -0400

    cleaned up TrainingViewer module

--- src/UI/TrainingViewer.jsx
+++ src/UI/TrainingViewer.jsx
@@ -1,34 +1,45 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import FileDropdown from "./ViewerComponents/FileDropdown";
-import ProgressFileReader from "./ViewerComponents/ProgressFileReader";
+import axios from "axios"
+import FileDropdown from "./ViewerComponents/FileDropdown"
+import ProgressFileReader from "./ViewerComponents/ProgressFileReader"
+import React from "react"
 
+import { useState } from "react"
 
 const TrainingViewer = ({}) => {
+
+  const [selectedFile, setSelectedFile] = useState("")
+  
   const startTensorBoard = () => {
-    // URL of the Flask endpoint
+    
+    // URL of Flask endpoint.
     axios.get('http://localhost:5005/start-tensorboard')
       .then(response => {
-        // Handle response here
-        alert(response.data.message);
-        // Opens TensorBoard in a new tab
-        window.open('http://localhost:6006', '_blank');
+        
+        // Display response data message.
+        alert(response.data.message)
+        
+        // Open TensorBoard in a new tab.
+        window.open('http://localhost:6006', '_blank')
       })
       .catch(error => {
-        // Handle error here
-        console.error('Error starting TensorBoard:', error);
-        alert('Failed to start TensorBoard. Check console for more details.');
-      });
-  };
-  const [selectedFile, setSelectedFile] = useState("");
+        
+        // Log TensorBoard error.
+        console.error('Error starting TensorBoard: ', error)
+
+        // Display TensorBoard error occured.
+        alert('Failed to start TensorBoard. Check console for more details.')
+      })
+  }
+
   return (
     <>
-      <button onClick={startTensorBoard}>Start TensorBoard</button>
-      <FileDropdown onSelectFile={setSelectedFile} />
-      {selectedFile && <ProgressFileReader fileName={selectedFile} />}
+      <button onClick = { startTensorBoard }>Start TensorBoard</button>
+      
+      <FileDropdown onSelectFile = { setSelectedFile } />
+      
+      { selectedFile && <ProgressFileReader fileName = { selectedFile } /> }
     </>
-  );
-};
+  )
+}
 
-export default TrainingViewer;
+export default TrainingViewer
