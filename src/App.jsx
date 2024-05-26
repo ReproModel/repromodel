6d0c0c2630bf16efe12cdcc71830af3faf25c2c9
tomonarifeehan <feehan.tomonari@gmail.com>
2024-05-26 17:11:29 -0400

    rename to repromodel-structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Button, ButtonGroup } from "@mui/material";
 import "./App.css";
 
 import Header from "./components/header/header"
-import ModuleStack from "./components/module-stack/module-stack"
+import RepromodelStructure from "./components/repromodel-structure/repromodel-structure"
 import newQuestions from "../repromodel_core/choices.json";
 import { handleFileChange, handleSubmit } from "./helperFunctions/FormHelper";
 import DynamicFormBuilder from "./UI/DynamicFormBuilder";
@@ -32,7 +32,7 @@ function App() {
           <Grid container direction={"row"}>
             <Grid item xs={4} className="stackContainer">
               <Grid item className="stackFrame">
-                <ModuleStack FormikProps = { FormikProps } />
+                <RepromodelStructure FormikProps = { FormikProps } />
               </Grid>
             </Grid>
             <Grid item xs={8} className="questionairContainer">
