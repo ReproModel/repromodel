71b4fc1a63f8509a39509456abf569c7e9ff58d5
tomonarifeehan <feehan.tomonari@gmail.com>
2024-05-26 13:03:46 -0400

    cleaned up field types components

--- src/UI/FormComponents/FieldTypes/FolderQuestion.jsx
+++ src/UI/FormComponents/FieldTypes/FolderQuestion.jsx
@@ -1,43 +1,48 @@
-import React from "react";
-import { Field } from "formik";
-import CustomSelect from "./CustomSelectComponent";
+import CustomSelect from "./CustomSelectComponent"
+import React from "react"
 
 const extractSecondLevelKeys = (obj) => {
-  const options = [];
+  
+  const options = []
 
   const traverse = (obj, level = 0) => {
+    
     if (level === 1) {
       Object.keys(obj).forEach((key) => {
-        options.push({ value: key, label: key });
-      });
+        options.push({ value: key, label: key })
+      })
+    
     } else {
       Object.values(obj).forEach((value) => {
         if (typeof value === "object") {
-          traverse(value, level + 1);
+          traverse(value, level + 1)
         }
-      });
+      })
     }
-  };
+  }
 
-  traverse(obj);
-  return options;
-};
+  traverse(obj)
+
+  return options
+}
 
 export function SmartFolderField({ folder, folderContent }) {
-  // Define the array of specific folder names
-  const multipleSelectFolders = ["models", "metrics"];
+  
+  // Define the array of specific folder names.
+  const multipleSelectFolders = ["models", "metrics"]
 
-  // Convert folderContent into Array
-  const newoptions = extractSecondLevelKeys(folderContent);
+  // Convert folderContent into array.
+  const newoptions = extractSecondLevelKeys(folderContent)
 
-  // Check if the folder name is within the specific folders array
-  const isMultipleFolder = multipleSelectFolders.includes(folder);
+  // Check if the folder name is within the specific folders array.
+  const isMultipleFolder = multipleSelectFolders.includes(folder)
+  
   return (
     <CustomSelect
-      name={folder}
-      options={newoptions}
-      placeholder="Select or start typing"
-      isMulti={isMultipleFolder}
+      name = { folder }
+      options = { newoptions }
+      placeholder = "Select or start typing..."
+      isMulti = { isMultipleFolder }
     />
-  );
+  )
 }
--- src/UI/FormComponents/FieldTypes/SmartStringField.jsx
+++ src/UI/FormComponents/FieldTypes/SmartStringField.jsx
@@ -1,38 +1,40 @@
-import React from "react";
-import { Field} from "formik";
+import React from "react"
+
+import { Field } from "formik"
 
 function SmartFreeTextField({ id, label, object, name }) {
-  // Attempt to parse the options from the object, if they exist
-  const optionsArray = object?.options
-    ? JSON.parse(object.options.replace(/'/g, '"'))
-    : [];
-
-  const defaultValue = object?.default ? `Standard would be: ${object.default}` : "";  
-
- 
+  
+  // Attempt to parse the options from the object, if they exist.
+  const optionsArray = object?.options ? JSON.parse(object.options.replace(/'/g, '"')) : []
 
   return (
     <>
-      {optionsArray.length > 0 ? (
-        <Field as="select" className="inputField" id={id} name={name}>
+      { optionsArray.length > 0 ? (
+        
+        <Field as = "select" className = "inputField" id = { id } name = { name }>
           <>
-            <option value="">Select an option</option>
-            {optionsArray.map((option, index) => (
-              <option key={index} value={option}>
-                {option}
+            <option value = "">Select an option...</option>
+            
+            { optionsArray.map((option, index) => (
+              
+              <option key = { index } value = { option }>
+                { option }
               </option>
+            
             ))}
           </>
         </Field>
+
       ) : (
         <Field
-          className="inputField"
-          type="text"
-          id={id}
-          name={name}
+          className = "inputField"
+          type = "text"
+          id = { id }
+          name = { name }
         />
       )}
     </>
-  );
+  )
 }
-export default SmartFreeTextField;
+
+export default SmartFreeTextField
