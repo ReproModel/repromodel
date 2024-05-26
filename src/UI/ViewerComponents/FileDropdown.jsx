b03bb7482ff3b316d94136a1f4c24234e1a34ef2
tomonarifeehan <feehan.tomonari@gmail.com>
2024-05-26 12:31:19 -0400

    cleaned up viewer components

--- src/UI/ViewerComponents/FileDropdown.jsx
+++ src/UI/ViewerComponents/FileDropdown.jsx
@@ -1,50 +1,54 @@
-// src/components/FileDropdown.js
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { FormControl, InputLabel, Select, MenuItem, Box } from '@mui/material';
+import axios from 'axios'
+import React from 'react'
+
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { useEffect, useState } from 'react'
+
+const handleChange = (event) => {
+  const value = event.target.value
+  setSelectedFile(value)
+  onSelectFile(value)
+}
 
 const FileDropdown = ({ onSelectFile }) => {
-  const [files, setFiles] = useState([]);
-  const [selectedFile, setSelectedFile] = useState('');
+  
+  const [files, setFiles] = useState([])
+  const [selectedFile, setSelectedFile] = useState('')
 
   useEffect(() => {
     const fetchFiles = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:5005/api/files');
+        const response = await axios.get('http://127.0.0.1:5005/api/files')
         setFiles(response.data);
       } catch (error) {
-        console.error('Error fetching files:', error);
+        console.error('Error fetching files: ', error)
       }
-    };
-
-    fetchFiles();
-  }, []);
-
-  const handleChange = (event) => {
-    const value = event.target.value;
-    setSelectedFile(value);
-    onSelectFile(value);
-  };
+    }
+    fetchFiles()
+  }, [])
 
   return (
-    <Box sx={{ width: 300, mt: 4 }}>
+    <Box sx = { { width: 300, mt: 4 } }>
+      
       <FormControl fullWidth>
-        <InputLabel id="file-select-label">Output File</InputLabel>
+        
+        <InputLabel id = "file-select-label">Output File</InputLabel>
+        
         <Select
-          labelId="file-select-label"
-          value={selectedFile}
-          label="Select a file"
-          onChange={handleChange}
+          labelId = "file-select-label"
+          value = { selectedFile }
+          label = "Select a file"
+          onChange = { handleChange }
         >
-          {files.map((file, index) => (
-            <MenuItem key={index} value={file}>
-              {file}
-            </MenuItem>
+          { files.map((file, index) => (
+            <MenuItem key = { index } value = { file }>{ file }</MenuItem>
           ))}
         </Select>
-      </FormControl>
-    </Box>
-  );
-};
 
-export default FileDropdown;
+      </FormControl>
+
+    </Box>
+  )
+}
+
+export default FileDropdown
