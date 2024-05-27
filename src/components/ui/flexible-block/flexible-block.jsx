fa7f7df0bc9f15d3ba9b9e7232256c22035fa86d
tomonarifeehan <feehan.tomonari@gmail.com>
2024-05-27 11:56:10 -0400

    move flexible block to ui folder

--- src/components/ui/flexible-block/flexible-block.jsx
+++ src/components/ui/flexible-block/flexible-block.jsx
@@ -2,7 +2,7 @@ import "./flexible-block.css"
 
 import React from "react"
 
-import { capitalizeFirstLetter } from "../../utils/string-helpers"
+import { capitalizeFirstLetter } from "../../../utils/string-helpers"
 import { Paper, Typography } from "@mui/material"
 
 function ActiveBlock({ id, name, status }) {
