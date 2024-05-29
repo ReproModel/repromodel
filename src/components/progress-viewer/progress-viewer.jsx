87d3526482bfd71b29f52fddff95c6a322126409
tomonarifeehan <feehan.tomonari@gmail.com>
2024-05-29 11:38:13 -0400

    moved FileReader component

--- src/components/progress-viewer/progress-viewer.jsx
+++ src/components/progress-viewer/progress-viewer.jsx
@@ -2,7 +2,7 @@ import "./progress-viewer.css"
 
 import axios from "axios"
 import FileDropdown from "../ui/file-dropdown"
-import ProgressFileReader from "./progress-file-reader"
+import FileReader from "../ui/file-reader/file-reader"
 import React from "react"
 
 import { Typography } from "@mui/material"
@@ -15,22 +15,22 @@ const ProgressViewer = ({}) => {
   const startTensorBoard = () => {
     
     // Send GET request to /start-tensorboard Flask endpoint.
-    axios.get('http://localhost:5005/start-tensorboard')
+    axios.get("http://localhost:5005/start-tensorboard")
       .then(response => {
         
         // Display response data message.
         alert(response.data.message)
         
         // Open TensorBoard in a new tab.
-        window.open('http://localhost:6006', '_blank')
+        window.open("http://localhost:6006", "_blank")
       })
       .catch(error => {
         
         // Log TensorBoard error in console.
-        console.error('Error starting TensorBoard: ', error)
+        console.error("Error starting TensorBoard: ", error)
 
         // Display TensorBoard error occured.
-        alert('Failed to start TensorBoard. Check console for more details.')
+        alert("Failed to start TensorBoard. Check console for more details.")
       })
   }
 
@@ -40,9 +40,9 @@ const ProgressViewer = ({}) => {
         <Typography>Start TensorBoard</Typography>
       </button>
       
-      <FileDropdown onSelectFile = { setSelectedFile } />
+      <FileDropdown onSelectFile = { setSelectedFile }/>
     
-      { selectedFile && <ProgressFileReader fileName = { selectedFile } /> }
+      { selectedFile && <FileReader fileName = { selectedFile }/> }
     </>
   )
 }
