a5fa110ae57767654e7061a635640eb7ce7c31db
tomonarifeehan <feehan.tomonari@gmail.com>
2024-06-04 14:44:06 -0400

    clean up code / remove dead code

--- src/components/status-checks/status-checks.jsx
+++ src/components/status-checks/status-checks.jsx
@@ -1,56 +1,60 @@
-import React, { useState, useEffect} from 'react';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import axios from 'axios';
-import './status-checks.css';
-import trainConfig from "../../../repromodel_core/train_config.json"
+import "./status-checks.css"
 
+import axios from "axios"
+import React from "react"
+
+import { useEffect, useState } from "react"
 
 const StatusCheck = () => {
     
-    const [isBackendActive, setIsBackendActive] = useState(false);
+  const [isBackendActive, setIsBackendActive] = useState(false)
 
   useEffect(() => {
+    
     const interval = setInterval(() => {
+      
       axios.get("http://127.0.0.1:5005/ping")
+        
         .then(response => {
           if (response.status === 200) {
-            setIsBackendActive(true);
+            setIsBackendActive(true)
           } else {
-            setIsBackendActive(false);
+            setIsBackendActive(false)
           }
-        })
-        .catch(error => {
-          setIsBackendActive(false);
-        });
-    }, 3000);
+        })      
 
-    return () => clearInterval(interval);
-  }, []);
+        .catch(error => {
+          setIsBackendActive(false)
+        })
+
+    }, 3000)
+
+    return () => clearInterval(interval)
+
+  }, [])
     
     
 
   return (
-    <div className="status-wrapper-position">
+    <div className = "status-wrapper-position">
 
-      <div className='blinking'
-        style={{
-          width: '10px',
-          height: '10px',
-          borderRadius: '50%',
-          backgroundColor: isBackendActive ? 'green' : 'red',
-          marginRight: '10px', 
-          marginLeft: "8px",
+      <div className = "blinking"
+        style = { {
+          width: "10px",
+          height: "10px",
+          borderRadius: "50%",
+          backgroundColor: isBackendActive ? "green" : "red",
+          marginRight: "10px", 
+          marginLeft: "8px"
         }}
       />
+
       <span>
-        {isBackendActive ? 'Backend Active' : 'Backend Offline'}
+        { isBackendActive ? "Backend Active" : "Backend Offline" }
       </span>
     
     </div>
-  );
-};
+  )
+}
 
-export default StatusCheck;
+export default StatusCheck
