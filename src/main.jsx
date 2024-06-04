29c14f34d4aa81edf822e377e446dc9bf36b26b9
tomonarifeehan <feehan.tomonari@gmail.com>
2024-06-04 15:01:12 -0400

    reset main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ModalitySection from './components/modality-section/modality-section'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ModalitySection/>
+    <App/>
   </React.StrictMode>
 )
