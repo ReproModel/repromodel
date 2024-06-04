9fd0f500e7d6f8cff4d6ea1670f8b49d41871776
tomonarifeehan <feehan.tomonari@gmail.com>
2024-06-04 16:06:50 -0400

    initial commit / file upload

--- src/components/custom-script/custom-script.jsx
+++ src/components/custom-script/custom-script.jsx
@@ -1,11 +1,13 @@
 import CodeEditor from "../ui/code-editor/code-editor"
 
+import { Form } from "formik"
+
 const CustomScript = ({}) => {
 
   return (
-    <>
+    <Form>
       <CodeEditor label = "Create Custom Script"/>
-    </>
+    </Form>
   )
 }
 
--- src/components/llm-description/llm-description.jsx
+++ src/components/llm-description/llm-description.jsx
@@ -5,7 +5,7 @@ import React from "react"
 import { Form } from "formik"
 import { Typography } from "@mui/material"
 
-const LLMDescription = ({ FormikProps, handleFileChange }) => {
+const LLMDescription = ({ FormikProps, handleFileChange, setFieldValue }) => {
 
   return (
     <Form>
