056cf198f6459b4d6e971fa7ae0ac146764201f2
tomonarifeehan <feehan.tomonari@gmail.com>
2024-06-05 12:07:53 -0400

    added LaTeX editor and states

--- src/components/llm-description/llm-description.jsx
+++ src/components/llm-description/llm-description.jsx
@@ -1,21 +1,48 @@
-import "./llm-description.css"
-
-import CustomSelectComponent from "../ui/custom-select"
-import React from "react"
-
+import { exampleLatexOutput, examplePlainTextOutput } from "./example-response"
 import { Form } from "formik"
 import { FormControlLabel, Radio, RadioGroup, TextareaAutosize, Typography } from "@mui/material"
+import { highlight, languages } from "prismjs/components/prism-core"
+
+import CustomSelectComponent from "../ui/custom-select"
+import Editor from "react-simple-code-editor"
+import React from "react"
+
+import "./llm-description.css"
+import "prismjs/components/prism-clike"
+import "prismjs/components/prism-latex"
+import "prismjs/themes/prism.css"
 
 const supportedLLMs = [
-  { index: 0, label: "Llama-3-8b", company: "Meta", image: "./llm-models/meta.svg" },
-  { index: 1, label: "Llama-3-70b", company: "Meta", image: "./llm-models/meta.svg" },
-  { index: 3, label: "Mistral-7b", company: "Mistral AI,", image: "./llm-models/mistal.svg" }
+  { value: "Llama-3-70b", label: <div><img src =  "./llm-models/meta.svg" height = "12px" width = "20px"/>{" "}Llama-3-70b</div> },
+  { value: "Llama-3-8b", label: <div><img src =  "./llm-models/meta.svg" height = "12px" width = "20px"/>{" "}Llama-3-8b</div> },
+  { value: "Mistral-7b", label: <div><img src =  "./llm-models/mistral.svg" height = "12px" width = "20px"/>{" "}Mistral-7b</div> }
 ]
 
 const LLMDescription = ({ handleFileChange, setFieldValue }) => {
 
+  // Radio Button - Writing Voice
+  const [voice, setVoice] = React.useState("passive")
+  
+  // Radio Button - Output File Format
+  const [format, setFormat] = React.useState("latex")
+
+  // LLM Output
+  const [output, setOutput] = React.useState("")
+
   const runQuery = () => { 
     console.log("Running LLM query...")
+
+    console.log("Radio Radio - Writing Voice: ", voice)
+    console.log("Radio Radio - Output File Format: ", format)
+
+    // Temporary Hard-coded example (DELETE LATER)
+    if (format == "latex") {
+      setOutput(exampleLatexOutput)
+    }
+    
+    else if (format == "plain-text") {
+      setOutput(examplePlainTextOutput)
+    }
   }
 
   return (
@@ -60,12 +87,12 @@ const LLMDescription = ({ handleFileChange, setFieldValue }) => {
             
           <TextareaAutosize
             className = "textarea-additional-prompt"
-            placeholder = ""
+            placeholder = "• Describe my custom model in more detail while only briefly describe other models that are used.&#10;&#10;• Do not describe optimizers and LR schedulers."
             minRows = { 4 }
             style = { { width: "100%", height: "40px" } }
             sx = { {
-              '&::before': { display: 'none' },
-              '&:focus-within': { outline: '2px solid var(--Textarea-focusedHighlight)', outlineOffset: '2px' }
+              "&::before": { display: "none" },
+              "&:focus-within": { outline: "2px solid var(--Textarea-focusedHighlight)", outlineOffset: "2px" }
             }}
           />
           
@@ -77,11 +104,11 @@ const LLMDescription = ({ handleFileChange, setFieldValue }) => {
                
           <Typography className = "radio-writing-voice-label">Writing Voice</Typography>
             
-          <RadioGroup className = "radio-writing-voice" row>
+          <RadioGroup className = "radio-writing-voice" onChange = { (e, value) => { setOutput(""); setVoice(value); }} row>
             
-            <FormControlLabel value = "Passive Voice" control = { <Radio /> } label = "Passive Voice" />
+            <FormControlLabel value = "passive" control = { <Radio /> } label = "Passive Voice" />
             
-            <FormControlLabel value = "Active Voice" control = { <Radio /> } label = "Active Voice" />
+            <FormControlLabel value = "active" control = { <Radio /> } label = "Active Voice" />
           
           </RadioGroup>
 
@@ -93,19 +120,41 @@ const LLMDescription = ({ handleFileChange, setFieldValue }) => {
           
           <Typography className = "radio-output-format-label">Output Format</Typography>
             
-          <RadioGroup className = "radio-output-format" row>
+          <RadioGroup className = "radio-output-format" onChange = { (e, value) => { setOutput(""); setFormat(value); }} row>
             
-            <FormControlLabel value = "LaTeX (.tex)" control = { <Radio /> } label = "LaTeX (.tex)" />
+            <FormControlLabel value = "latex" control = { <Radio/> } label = "LaTeX (.tex)" />
             
-            <FormControlLabel value = "Plain Text (.txt)" control = { <Radio /> } label = "Plain Text (.txt)" className = "radio-plain-text" />
+            <FormControlLabel value = "plain-text" control = { <Radio/> } label = "Plain Text (.txt)" className = "radio-plain-text" />
           
           </RadioGroup>
 
         </div>
 
         {/* Submit Button */}
-        <button className = "submit-button" onClick = { runQuery() }>
+        <button className = "submit-button" onClick = { (e) => { runQuery() } }>
           <Typography>Submit</Typography>
+        </button>
+
+        {/* LLM Response Output */}
+        <div className = "textarea-llm-response-container" style = { { width: "100%" } }>
+
+          <Typography className = "textarea-llm-response-label">LLM Response</Typography>
+
+          <div className = "container-content" style = {{width: "100%"}}>
+            <div className = "container-editor-area">
+              <Editor
+                className = "container-editor"
+                value = { output }
+                onValueChange = { (output) => setOutput(output) }
+                highlight = { (output) => (format == "latex") ? highlight(output, languages.latex) : highlight(output, languages.text) }
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Copy Button */}
+        <button type = "submit" className = "copy-button" onClick = { () => navigator.clipboard.writeText(code) }>
+          Copy
         </button>
 
     </Form>
