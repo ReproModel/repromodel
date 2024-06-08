75f817e3b7effbbc0a33ef07b3857fb5b7977cee
tomonarifeehan <feehan.tomonari@gmail.com>
2024-06-08 15:04:07 -0400

    make loading animation only output and submit/copy

--- src/components/llm-description/llm-description.jsx
+++ src/components/llm-description/llm-description.jsx
@@ -19,7 +19,7 @@ const supportedLLMs = [
 
 const LLMDescription = ({ handleFileChange, setFieldValue }) => {
 
-  // Config
+  // LLM Option
   const [LLM, setLLM] = React.useState(supportedLLMs[0])
 
   // LLM Additional Prompt
@@ -81,125 +81,127 @@ const LLMDescription = ({ handleFileChange, setFieldValue }) => {
   return (
     <Form>
 
-        { isLoading ?  <l-tailspin size = "40" stroke = "5" speed = "0.9"  color = "black" className = "loading-animation" /> :
+      <Typography className = "heading">Generate the methods section for your research paper.</Typography>
+
+      
+      {/* Configuration File */}
+      <div className = "json-input-file-container">
+
+        <Typography className = "json-input-file-label">Upload existing configuration file to pass to LLM.</Typography>
         
-          <div>
-            <Typography className = "heading">Generate the methods section for your research paper.</Typography>
+        <input
+          type = "file"
+          className = "json-input-file"
+          accept = ".json"
+          onChange = { (event) => handleFileChange(event, setFieldValue) }
+        />
 
+      </div>
+      
+      {/* Supported LLM Models */}
+      <div className = "supported-llm-container">
+        
+        <Typography className = "supported-llm-label">LLM Model</Typography>
+
+        <CustomSelectComponent
+          className = "supported-llm"
+          placeholder = "Select model..."
+          isMulti = { false }
+          options = { supportedLLMs }
+          value = { LLM }
+          onChange = { (option) => { setLLM(option) } }
+        />
+
+      </div>
+
+      
+      {/* Additional LLM Prompting Rules */}
+      <div className = "textarea-additional-prompt-container" style = { { width: "100%" } }>
+
+        <Typography className = "textarea-additional-prompt-label">Additional LLM Prompting Rules</Typography>
+          
+        <TextareaAutosize
+          className = "textarea-additional-prompt"
+          onChange = { (e) => { setAdditionalPrompt(e.target.value) } }
+          value = { additionalPrompt }
+          placeholder = "• Describe my custom model in more detail while only briefly describe other models that are used.&#10;&#10;• Do not describe optimizers and LR schedulers."
+          minRows = { 4 }
+          style = { { width: "100%", height: "40px" } }
+          sx = { {
+            "&::before": { display: "none" },
+            "&:focus-within": { outline: "2px solid var(--Textarea-focusedHighlight)", outlineOffset: "2px" }
+          }}
+        />
+        
+      </div>
+      
+      
+      {/* Writing Voice */}
+      <div className = "radio-writing-voice-container">
             
-            {/* Configuration File */}
-            <div className = "json-input-file-container">
+        <Typography className = "radio-writing-voice-label">Writing Voice</Typography>
+          
+        <RadioGroup className = "radio-writing-voice" onChange = { (e, value) => { setOutput(""); setVoice(value); }} row>
+          
+          <FormControlLabel value = "passive" control = { <Radio checked = { voice == "passive" }/> } label = "Passive Voice" />
+          
+          <FormControlLabel value = "active" control = { <Radio checked = { voice == "active" }/> } label = "Active Voice" />
+        
+        </RadioGroup>
 
-              <Typography className = "json-input-file-label">Upload existing configuration file to pass to LLM.</Typography>
-              
-              <input
-                type = "file"
-                className = "json-input-file"
-                accept = ".json"
-                onChange = { (event) => handleFileChange(event, setFieldValue) }
-              />
-
-            </div>
-            
-            {/* Supported LLM Models */}
-            <div className = "supported-llm-container">
-              
-              <Typography className = "supported-llm-label">LLM Model</Typography>
-
-              <CustomSelectComponent
-                className = "supported-llm"
-                placeholder = "Select model..."
-                isMulti = { false }
-                options = { supportedLLMs }
-                value = { LLM }
-                onChange = { (option) => { setLLM(option) } }
-              />
-
-            </div>
-
-            
-            {/* Additional LLM Prompting Rules */}
-            <div className = "textarea-additional-prompt-container" style = { { width: "100%" } }>
-
-              <Typography className = "textarea-additional-prompt-label">Additional LLM Prompting Rules</Typography>
-                
-              <TextareaAutosize
-                className = "textarea-additional-prompt"
-                onChange = { (e) => { setAdditionalPrompt(e.target.value) } }
-                value = { additionalPrompt }
-                placeholder = "• Describe my custom model in more detail while only briefly describe other models that are used.&#10;&#10;• Do not describe optimizers and LR schedulers."
-                minRows = { 4 }
-                style = { { width: "100%", height: "40px" } }
-                sx = { {
-                  "&::before": { display: "none" },
-                  "&:focus-within": { outline: "2px solid var(--Textarea-focusedHighlight)", outlineOffset: "2px" }
-                }}
-              />
-              
-            </div>
-            
-            
-            {/* Writing Voice */}
-            <div className = "radio-writing-voice-container">
-                  
-              <Typography className = "radio-writing-voice-label">Writing Voice</Typography>
-                
-              <RadioGroup className = "radio-writing-voice" onChange = { (e, value) => { setOutput(""); setVoice(value); }} row>
-                
-                <FormControlLabel value = "passive" control = { <Radio checked = { voice == "passive" }/> } label = "Passive Voice" />
-                
-                <FormControlLabel value = "active" control = { <Radio checked = { voice == "active" }/> } label = "Active Voice" />
-              
-              </RadioGroup>
-
-            </div>
+      </div>
 
 
-            {/* Output Format */}
-            <div className = "radio-output-format-container">
-              
-              <Typography className = "radio-output-format-label">Output Format</Typography>
-                
-              <RadioGroup className = "radio-output-format" onChange = { (e, value) => { setOutput(""); setFormat(value); }} row>
-                
-                <FormControlLabel value = "latex" control = { <Radio checked = { format == "latex" }/> } label = "LaTeX (.tex)" />
-                
-                <FormControlLabel value = "plain-text" control = { <Radio checked = { format == "plain-text" }/> } label = "Plain Text (.txt)" className = "radio-plain-text" />
-              
-              </RadioGroup>
+      {/* Output Format */}
+      <div className = "radio-output-format-container">
+        
+        <Typography className = "radio-output-format-label">Output Format</Typography>
+          
+        <RadioGroup className = "radio-output-format" onChange = { (e, value) => { setOutput(""); setFormat(value); }} row>
+          
+          <FormControlLabel value = "latex" control = { <Radio checked = { format == "latex" }/> } label = "LaTeX (.tex)" />
+          
+          <FormControlLabel value = "plain-text" control = { <Radio checked = { format == "plain-text" }/> } label = "Plain Text (.txt)" className = "radio-plain-text" />
+        
+        </RadioGroup>
 
-            </div>
+      </div>
+      
+       {/* Loading Animation */}
+      { isLoading ?  <l-tailspin size = "40" stroke = "5" speed = "0.9"  color = "black" className = "loading-animation" /> :
+        
+        <div>
 
-            {/* Submit Button */}
-            <button className = "submit-button" type="button" onClick = { () => runQuery("Method is bruteforce", additionalPrompt, voice, format) }>
-              <Typography>Submit</Typography>
-            </button>
+          {/* Submit Button */}
+          <button className = "submit-button" type = "button" onClick = { () => runQuery("Method is bruteforce", additionalPrompt, voice, format) }>
+            <Typography>Submit</Typography>
+          </button>
 
-            {/* LLM Response Output */}
-            <div className = "textarea-llm-response-container" style = { { width: "100%" } }>
+          {/* LLM Response Output */}
+          <div className = "textarea-llm-response-container" style = { { width: "100%" } }>
 
-              <Typography className = "textarea-llm-response-label">LLM Response</Typography>
+            <Typography className = "textarea-llm-response-label">LLM Response</Typography>
 
-              <div className = "container-content" style = {{width: "100%"}}>
-                <div className = "container-editor-area">
-                  <Editor
-                    className = "container-editor"
-                    value = { output }
-                    onValueChange = { (output) => setOutput(output) }
-                    highlight = { (output) => (format == "latex") ? highlight(output, languages.latex) : highlight(output, languages.text) }
-                  />
-                </div>
+            <div className = "container-content" style = {{width: "100%"}}>
+              <div className = "container-editor-area">
+                <Editor
+                  className = "container-editor"
+                  value = { output }
+                  onValueChange = { (output) => setOutput(output) }
+                  highlight = { (output) => (format == "latex") ? highlight(output, languages.latex) : highlight(output, languages.text) }
+                />
               </div>
             </div>
+          </div>
 
-            {/* Copy Button */}
-            <button type = "submit" className = "copy-button" onClick = { () => navigator.clipboard.writeText(output) }>
-              Copy
-            </button>
-            
-          </div>  
-        
-        }
+          {/* Copy Button */}
+          <button type = "submit" className = "copy-button" onClick = { () => navigator.clipboard.writeText(output) }>
+            Copy
+          </button>
+
+        </div>
+
+    }
 
     </Form>
   )
