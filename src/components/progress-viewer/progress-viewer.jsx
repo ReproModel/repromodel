e8520d0436c3555c6540bd294e27f2da168dc864
tomonarifeehan <feehan.tomonari@gmail.com>
2024-06-21 17:10:13 -0400

    restyle progress viewer and load command_output.txt automatically

--- src/components/progress-viewer/progress-viewer.jsx
+++ src/components/progress-viewer/progress-viewer.jsx
@@ -36,11 +36,34 @@ const ProgressViewer = ({}) => {
 
   return (
     <>
+      {/* Header - TensorBoard */}
+      <div className = "header-tensorboard">
+          <Typography variant = "h7" style = { { fontWeight: "600" } }>TensorBoard</Typography>
+      </div>
+
+      {/* Subheader - TensorBoard */}
+      <div className = "subheader-tensorboard">
+          <span style = { { fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif", fontSize: "12px" } }>A suite of visualization tools to understand, debug, and optimize TensorFlow programs for ML experimentation.</span>
+      </div>
+
       <button className = "tensorboard-button" onClick = { startTensorBoard }>
         <Typography>Start TensorBoard</Typography>
       </button>
       
-      <FileDropdown onSelectFile = { setSelectedFile }/>
+      {/* Header - View Output */}
+      <div className = "header-output">
+          <Typography variant = "h7" style = { { fontWeight: "600" } }>View Output</Typography>
+      </div>
+
+      {/* Subheader - View Output */}
+      <div className = "subheader-output">
+          <span style = { { fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif", fontSize: "12px" } }>View output logs and training progress.</span>
+      </div>
+      
+      {/* File Input - View Output */}
+      <div className = "file-input-output">
+        <FileDropdown onSelectFile = { setSelectedFile }/>
+      </div>
     
       { selectedFile && <FileReader fileName = { selectedFile }/> }
     </>
