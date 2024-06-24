5c38aaf7c5102c7c653e9709787b51c019a9cdbd
MintOwlTech <mintowltech@gmail.com>
2024-06-24 21:00:33 +0200

    fix kill process

--- src/components/status-checks/status-checks.jsx
+++ src/components/status-checks/status-checks.jsx
@@ -1,60 +1,89 @@
-import "./status-checks.css"
+import "./status-checks.css";
 
-import axios from "axios"
-import React from "react"
-
-import { useEffect, useState } from "react"
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
 
 const StatusCheck = () => {
-    
-  const [isBackendActive, setIsBackendActive] = useState(false)
+  const [isBackendActive, setIsBackendActive] = useState(false);
+  const [isTrainingInProgress, setIsTrainingInProgress] = useState(false);
+  const [isTestingInProgress, setIsTestingInProgress] = useState(false);
 
   useEffect(() => {
-    
     const interval = setInterval(() => {
-      
-      axios.get("http://127.0.0.1:5005/ping")
-        
-        .then(response => {
+      axios
+        .get("http://127.0.0.1:5005/ping")
+        .then((response) => {
           if (response.status === 200) {
-            setIsBackendActive(true)
+            setIsBackendActive(true);
+            setIsTrainingInProgress(response.data.trainingInProgress);
+            setIsTestingInProgress(response.data.testingInProgress);
           } else {
-            setIsBackendActive(false)
+            setIsBackendActive(false);
+            setIsTrainingInProgress(false);
+            setIsTestingInProgress(false);
           }
-        })      
-
-        .catch(error => {
-          setIsBackendActive(false)
         })
+        .catch((error) => {
+          setIsBackendActive(false);
+          setIsTrainingInProgress(false);
+          setIsTestingInProgress(false);
+        });
+    }, 3000);
 
-    }, 3000)
-
-    return () => clearInterval(interval)
-
-  }, [])
-    
-    
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className = "status-wrapper-position">
+    <>
+      <div className="status-wrapper-position">
+        <div
+          className="blinking"
+          style={{
+            width: "10px",
+            height: "10px",
+            borderRadius: "50%",
+            backgroundColor: isBackendActive ? "green" : "red",
+            marginRight: "10px",
+            marginLeft: "8px",
+          }}
+        />
+        <span>{isBackendActive ? "Backend Active" : "Backend Offline"}</span>
+      </div>
+      {isTrainingInProgress && (
+        <div className="status-wrapper-position position2">
+          <div
+            className="blinking"
+            style={{
+              width: "10px",
+              height: "10px",
+              borderRadius: "50%",
+              backgroundColor: "blue",
+              marginRight: "10px",
+              marginLeft: "8px",
+            }}
+          />
+          <span>Training Runs</span>
+        </div>
+      )}
+      {isTestingInProgress && (
+        <div className="status-wrapper-position position2">
+          <div
+            className="blinking"
+            style={{
+              width: "10px",
+              height: "10px",
+              borderRadius: "50%",
+              backgroundColor: "blue",
+              marginRight: "10px",
+              marginLeft: "8px",
+            }}
+          />
+          <span>Testing Runs</span>
+        </div>
+      )}
+    </>
+  );
+};
 
-      <div className = "blinking"
-        style = { {
-          width: "10px",
-          height: "10px",
-          borderRadius: "50%",
-          backgroundColor: isBackendActive ? "green" : "red",
-          marginRight: "10px", 
-          marginLeft: "8px"
-        }}
-      />
-
-      <span>
-        { isBackendActive ? "Backend Active" : "Backend Offline" }
-      </span>
-    
-    </div>
-  )
-}
-
-export default StatusCheck
+export default StatusCheck;
