4d1e1921d24b72e0e7d997545f9bbfbd67c650b4
tomonarifeehan <feehan.tomonari@gmail.com>
2024-07-26 21:23:01 -0400

    Clean up mobile warning component.

--- src/components/mobile/mobile-warning.jsx
+++ src/components/mobile/mobile-warning.jsx
@@ -1,17 +1,23 @@
+import "./mobile-warning.css"
+
 import React from 'react'
 
 const MobileWarning = () => {
 
   return (
     
-    <div className = "container">
+    <div className = "mobile-warning-container">
+
+      <div className = "mobile-warning-image-container">
       
-      <h1 className = "header">Oops!</h1>
+        <h1 className = "mobile-warning-header">Oops!</h1>
+        
+        <p className = "mobile-warning-message">ReproModel is currently only built for Desktop use.</p>
+        <p className = "mobile-warning-message">Looks like you're trying to access it on a mobile device. Trust us, it's way better on a big screen!</p>
+        
+        <p className = "mobile-warning-footer">Please visit us from a desktop for the full experience.</p>
       
-      <p className = "message">ReproModel is currently only built for Desktop use.</p>
-      <p className = "message">Looks like you're trying to access it on a mobile device. Trust us, it's way better on a big screen!</p>
-      
-      <p className = "footer">Please visit us from a desktop for the full experience.</p>
+      </div>
     
     </div>
   )
